96fbbe893350c572e5fda5e621c3f67d96d5072b
tflinch <tflinch16@gmail.com>
2024-06-24 22:09:53 -0400

    feat: updated components, add public assets

---
+++ src/components/Content.jsx
@@ -0,0 +1,17 @@
+const Content = () =>{
+
+    return(
+        <div className='content-container'>
+        <span className='title'>PERFUME</span>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+        <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the house of CHANEL.</p>
+        <div className='price-container'>
+          <h3>$149.99</h3>
+          <span>169.99</span>
+        </div>
+        <button> <img src="/assets/images/icon-cart.svg" alt="cart"/> Add to Cart</button>
+        </div>
+    )
+}
+
+export default Content
---
+++ src/components/Image.jsx
@@ -0,0 +1,10 @@
+const Image =()=>{
+
+    return(
+        <div className='img-container'>
+        <img src="/assets/images/image-product-desktop.jpg" alt="parfum bottle" />
+        </div>
+    )
+}
+
+export default Image;
