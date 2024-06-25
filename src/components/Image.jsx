016e943e72582921dd6ea67d28ebc5bfacc2f424
tflinch <tflinch16@gmail.com>
2024-06-24 23:05:49 -0400

    feat: update image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,7 +2,7 @@ const Image =()=>{
 
     return(
         <div className='img-container'>
-        <img src="/assets/images/image-product-desktop.jpg" alt="parfum bottle" />
+        <img src="https://i.imgur.com/xBhpn79.jpeg" alt="parfum bottle" />
         </div>
     )
 }
